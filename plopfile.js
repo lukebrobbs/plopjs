dfcda047b888621f6fe539cfc0c0ba6ea539fa6b
Luke Brobbin <lukebrobbin@Lukes-MacBook-Air.local>
2019-04-04 09:15:58 +0100

    Added more templates

--- plopfile.js
+++ plopfile.js
@@ -9,22 +9,35 @@ module.exports = function(plop) {
       },
       {
         type: `confirm`,
+        name: `formik`,
+        message: `Will this component be using Formik?`
+      },
+      {
+        type: `confirm`,
         name: `addTestFile`,
         message: `Add a test file?`
       }
     ],
-    actions: data => [
-      {
-        type: `add`,
-        path: `src/components/{{kebabCase name}}/{{pascalCase name}}.js`,
-        templateFile: `plop-templates/classComponent.js.hbs`
-      },
-      data.addTestFile && {
-        type: `add`,
-        path: `src/components/{{kebabCase name}}/{{pascalCase name}}.spec.js`,
-        templateFile: `plop-templates/componentTest.js.hbs`
-      }
-    ]
+    actions: data =>
+      [
+        {
+          type: `add`,
+          path: `src/components/{{kebabCase name}}/{{pascalCase name}}.js`,
+          templateFile: data.formik
+            ? `plop-templates/javascript/formikClassComponent.js.hbs`
+            : `plop-templates/javascript/classComponent.js.hbs`
+        },
+        data.formik && {
+          type: `add`,
+          path: `src/components/{{kebabCase name}}/{{kebabCase name}}FormikSetup.js`,
+          templateFile: `plop-templates/javascript/formikSetup.js.hbs`
+        },
+        data.addTestFile && {
+          type: `add`,
+          path: `src/components/{{kebabCase name}}/{{pascalCase name}}.spec.js`,
+          templateFile: `plop-templates/javascript/componentTest.js.hbs`
+        }
+      ].filter(Boolean)
   });
 
   plop.setGenerator('Function component', {
@@ -37,21 +50,34 @@ module.exports = function(plop) {
       },
       {
         type: `confirm`,
+        name: `formik`,
+        message: `Will this component be using Formik?`
+      },
+      {
+        type: `confirm`,
         name: `addTestFile`,
         message: `Add a test file?`
       }
     ],
-    actions: data => [
-      {
-        type: `add`,
-        path: `src/components/{{kebabCase name}}/{{pascalCase name}}.js`,
-        templateFile: `plop-templates/functionComponent.js.hbs`
-      },
-      data.addTestFile && {
-        type: `add`,
-        path: `src/components/{{kebabCase name}}/{{pascalCase name}}.spec.js`,
-        templateFile: `plop-templates/componentTest.js.hbs`
-      }
-    ]
+    actions: data =>
+      [
+        {
+          type: `add`,
+          path: `src/components/{{kebabCase name}}/{{pascalCase name}}.js`,
+          templateFile: data.formik
+            ? `plop-templates/javascript/formikFunctionComponent.js.hbs`
+            : `plop-templates/javascript/functionComponent.js.hbs`
+        },
+        data.formik && {
+          type: `add`,
+          path: `src/components/{{kebabCase name}}/{{kebabCase name}}FormikSetup.js`,
+          templateFile: `plop-templates/javascript/formikSetup.js.hbs`
+        },
+        data.addTestFile && {
+          type: `add`,
+          path: `src/components/{{kebabCase name}}/{{pascalCase name}}.spec.js`,
+          templateFile: `plop-templates/javascript/componentTest.js.hbs`
+        }
+      ].filter(Boolean)
   });
 };

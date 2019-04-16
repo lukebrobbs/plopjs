5923411f6dc52409142e75d8e9b9e093a7ff53f5
Luke Brobbin <lukemichaelvocalist@gmail.com>
2019-04-16 19:46:26 +0100

    removed excess template files

--- plopfile.js
+++ plopfile.js
@@ -1,79 +1,25 @@
 module.exports = function(plop) {
-  plop.setGenerator('class Component', {
-    description: 'This creates a basic React class Component',
+  plop.setGenerator("class Component", {
+    description: "This creates a basic React class Component",
     prompts: [
       {
         type: `input`,
         name: `name`,
-        message: `name of new file and Component`
-      },
-      {
-        type: `confirm`,
-        name: `formik`,
-        message: `Will this component be using Formik?`
-      },
-      {
-        type: `confirm`,
-        name: `addTestFile`,
-        message: `Add a test file?`
+        message: `name of new Component`
       }
     ],
-    actions: data =>
+    actions: () =>
       [
         {
           type: `add`,
           path: `src/components/{{kebabCase name}}/{{pascalCase name}}.js`,
-          templateFile: data.formik
-            ? `plop-templates/javascript/formikClassComponent.js.hbs`
-            : `plop-templates/javascript/classComponent.js.hbs`
+          templateFile: `plop-templates/javascript/functionComponent.js.hbs`
         },
-        data.formik && {
-          type: `add`,
-          path: `src/components/{{kebabCase name}}/{{kebabCase name}}FormikSetup.js`,
-          templateFile: `plop-templates/javascript/formikSetup.js.hbs`
-        },
-        data.addTestFile && {
-          type: `add`,
-          path: `src/components/{{kebabCase name}}/{{pascalCase name}}.spec.js`,
-          templateFile: `plop-templates/javascript/componentTest.js.hbs`
-        }
-      ].filter(Boolean)
-  });
-
-  plop.setGenerator('Function component', {
-    description: 'This creates a basic React function Component',
-    prompts: [
-      {
-        type: `input`,
-        name: `name`,
-        message: `name of new file and Component`
-      },
-      {
-        type: `confirm`,
-        name: `formik`,
-        message: `Will this component be using Formik?`
-      },
-      {
-        type: `confirm`,
-        name: `addTestFile`,
-        message: `Add a test file?`
-      }
-    ],
-    actions: data =>
-      [
         {
           type: `add`,
-          path: `src/components/{{kebabCase name}}/{{pascalCase name}}.js`,
-          templateFile: data.formik
-            ? `plop-templates/javascript/formikFunctionComponent.js.hbs`
-            : `plop-templates/javascript/functionComponent.js.hbs`
+          path: `src/components/{{kebabCase name}}/{{kebabCase name}}.css`
         },
-        data.formik && {
-          type: `add`,
-          path: `src/components/{{kebabCase name}}/{{kebabCase name}}FormikSetup.js`,
-          templateFile: `plop-templates/javascript/formikSetup.js.hbs`
-        },
-        data.addTestFile && {
+        {
           type: `add`,
           path: `src/components/{{kebabCase name}}/{{pascalCase name}}.spec.js`,
           templateFile: `plop-templates/javascript/componentTest.js.hbs`
